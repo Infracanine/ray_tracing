9da33ee5f299d029ccfa6bc6bad135ea9656a258
Samuel Taylor-Hansen <samueltaylorhansen@gmail.com>
2020-08-02 12:14:09 +0100

    implemented some operations, methods, helper functions for Vec3 struct - may need redoing, but owrks for now

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,23 @@
 use std::fs::File;
 use std::io::prelude::*;
 
+mod vec3;
+
 fn main(){
-    // Image
+    // Image creation
     let image_width : u32 = 256;
     let image_height : u32 = 256;
-    let mut image = File::create("image.ppm").unwrap();
+    let mut image = File::create("image.ppm").expect("Failed to create image file!");
+
+    // Testing code goes here
+    // let test = vec3::Vec3{x:1.0,y:2.0,z:3.0};
+    // let test2 = vec3::Vec3{x:1.0,y:2.0,z:3.0};               
+    // let test3 = test / 2.0;
+    //println!("test vector is now: {:?}",test3);
+
 
     // Render
-    image.write(format!("P3\n{} {}\n255\n", image_width, image_height).as_bytes()).expect("panic!");
+    image.write(format!("P3\n{} {}\n255\n", image_width, image_height).as_bytes()).expect("Failed to write PNN prelude");
     for j in (0..image_height).rev(){
         eprintln!("\rScanlines remaining: {} ", j); // Prints simple progress indicator to error stream
         for i in (0..image_width).rev(){
@@ -16,9 +25,9 @@ fn main(){
             let g = j as f64 / (image_height - 1) as f64;
             let b:f64 = 0.25;
 
-            let ir:u32 = (255.999 * r) as u32;
-            let ig:u32 = (255.999 * g) as u32;
-            let ib:u32 = (255.999 * b) as u32;
+            let ir = (255.999 * r) as u32;
+            let ig = (255.999 * g) as u32;
+            let ib = (255.999 * b) as u32;
             image.write(format!("{} {} {}\n",ir,ig,ib).as_bytes()).expect("Failed to write a pixel!");
         }
     }
---
+++ src/vec3.rs
@@ -0,0 +1,98 @@
+use std::ops;
+
+#[derive(Debug)]
+pub struct Vec3{
+    pub x: f64,
+    pub y: f64,
+    pub z: f64
+
+}
+
+// Methods
+#[allow(dead_code)]
+impl Vec3{
+    // Operations on vectors
+    pub fn neg(&self) -> Vec3{
+        Vec3{x: -self.x,y: -self.y, z: -self.z}
+    }
+
+    pub fn length(&self) -> f64 {
+        self.length_squared().sqrt()
+    }
+    
+    pub fn length_squared(&self) -> f64 {
+        self.x * self.x + self.y * self.y + self.z * self.z
+    }
+}
+
+impl ops::Add for Vec3{
+    type Output = Vec3;
+
+    fn add(self, other: Self) -> Self {
+        Vec3 {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z
+        }
+    }
+}
+
+impl ops::Sub for Vec3{
+    type Output = Vec3;
+    
+    fn sub(self, other:Self) -> Self{
+        self + other.neg()
+    }
+}
+
+impl ops::Mul<f64> for Vec3{
+    type Output = Vec3;
+    fn mul(self, rhs: f64) -> Vec3{
+        Vec3{x: self.x * rhs,y: self.y * rhs, z: self.z * rhs}
+    }
+}
+
+impl ops::Mul<Vec3> for Vec3{
+    type Output = Vec3;
+    fn mul(self, rhs: Vec3) -> Vec3{
+        Vec3{x: self.x * rhs.x,y: self.y * rhs.y,z: self.z * rhs.z}
+    }
+
+}
+
+impl ops::Div<f64> for Vec3{
+    type Output = Vec3;
+    
+    fn div(self, rhs: f64) -> Vec3{
+        self * (1.0/rhs)
+    }
+}
+
+// Utility Functions
+
+pub fn zeroes() -> Vec3{
+    Vec3{x: 0.0,y: 0.0, z: 0.0}
+}
+
+pub fn new(X: f64, Y: f64, Z: f64) -> Vec3{
+    Vec3{
+        x: X,
+        y: Y, 
+        z: Z}
+}
+
+pub fn dot(u : &Vec3, v: &Vec3) -> f64{
+    (u.x * v.x) + (u.y * v.y) + (u.z * v.z)
+}
+
+pub fn cross(u: &Vec3, v: &Vec3) -> Vec3{
+    Vec3{
+        x: (u.y * v.z) - (u.z * v.y),
+        y: (u.z * v.x) - (u.x - v.z),
+        z: (u.x * v.y) - (u.y * v.x)}
+}
+
+pub fn unit_vector(u: &Vec3) -> Vec3{
+    let v = Vec3{x: u.x,y: u.y,z:u.z};
+    v / u.length()
+}
